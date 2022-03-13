bd00fadf16dc03efd14af3244cbeaf6a2f67db14
houris <faycelbelgacem93@gmail.com>
2022-03-13 13:46:51 +0100

    add login api

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userCtrl = require('../controllers/user');
 
 
 router.post('/signup' , userCtrl.signup);
-// router.post('/login', userCtrl.login);
+router.post('/login', userCtrl.login);
 
 
   module.exports = router ;
