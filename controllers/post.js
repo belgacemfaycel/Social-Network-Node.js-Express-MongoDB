b669b2c33c318fc488ec961401efadccd10ce004
houris <faycelbelgacem93@gmail.com>
2022-03-13 15:08:34 +0100

    edit/delete/getAll/getId posts

--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const Post = require('../models/Post');
-
-exports.createPost = (req, res, next) => {
+const fs = require('fs');
+    // add Post
+    exports.createPost = (req, res, next) => {
     delete req.body._id;
     const post = new Post({
       ...req.body,
@@ -12,4 +13,55 @@ exports.createPost = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));      
     };
 
-
+    // list all posts
+    exports.getPosts = (req, res, next) => {
+        Post.find()
+        .then(posts=>res.status(200).json(posts))
+        .catch(error => res.status(400).json({error}))
+      };
+      
+    // get post       
+      exports.getOnePosts = (req, res, next) => {
+        Post.findOne({_id : req.params.id})
+        .then(post=>res.status(200).json(post))
+        .catch(error => res.status(400).json({error}))
+      }
+    
+    // update post 
+      exports.updatePost = (req, res, next) => {
+        const postObject = req.file ?
+        {
+          ...JSON.parse(req.body.post),
+          imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+    
+        Post.updateOne({ _id: req.params.id }, { ...postObject, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'post updated !'}))
+        .catch(error => res.status(400).json({ error }));
+    
+      }
+     
+     // delete post  
+      exports.deletePost = (req, res, next) => {
+    
+        Post.findOne({_id : req.params.id})
+        .then(posts=>{
+          if(!posts){
+            return res.status(404).json({error : new Error('object not found')})
+          }
+          if(posts.userId !== req.auth.userId){
+            return res.status(401).json({error : new Error('requete non autorisé')})
+          }
+    
+          const filename = post.imageUrl.split('/images/')[1];
+          fs.unlink(`images/${filename}`, () => {
+            Post.deleteOne({ _id: req.params.id })
+              .then(() => res.status(200).json({ message: 'post deleted !'}))
+              .catch(error => res.status(400).json({ error }));
+    
+    
+          });
+        })
+        .catch(error => res.status(400).json({error}))
+    
+      };
--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,9 @@ const postCtrl = require('../controllers/post');
 const auth  = require('../middelware/auth');
 const multer = require('../middelware/multer-config');
 
-router.post('/' , auth ,multer ,postCtrl.createPost);
-
+  router.post('/' , auth ,multer ,postCtrl.createPost);
+  router.get('/',auth , postCtrl.getPosts);
+  router.get('/:id',auth , postCtrl.getOnePosts);
+  router.put('/:id', auth,multer ,postCtrl.updatePost);
+  router.delete('/:id',auth , postCtrl.deletePost);
   module.exports = router ;
